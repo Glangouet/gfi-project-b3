248b6094c4d5805b47709eaf9410b4d594d313e6
Maxime <maxime.egido@gmail.com>
2017-10-04 10:29:35 +0200

    front init

---
+++ src/GfiBundle/Resources/public/js/accueilController.js
@@ -0,0 +1,38 @@
+﻿angular.module("gfiApp").controller("accueilCtrl",
+    function ($scope, customersService) {
+
+    $scope.listeClients = [];
+
+    //$scope.listeClients = customersService.query({ id: $scope.user.userId }).$promise.then(function (result) {
+    //    $scope.listeClients = result;
+        
+    //    });
+    
+    
+    var cli1 = { nom: 'aaa', titre: 'aa', date: 'a' }
+    var cli2 = { nom: 'bbb', titre: 'bb', date: 'b' }
+    var cli3 = { nom: 'ccc', titre: 'cc', date: 'c' }
+    $scope.listeClients = [cli1, cli2, cli3];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
